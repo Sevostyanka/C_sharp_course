5af8c970dfea04d7d350141fde8144027c73eece
Ekaterina <sevic_bag@mail.ru>
2022-06-27 19:22:56 +0300

    все задания на семинаре

---
+++ 2_lesson/1-2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int MaxNam(int num)
+{
+    if (num % 10 > num / 10)
+    {
+        return num % 10;
+    }
+    else 
+        return num / 10;
+}
+
+int SecondNum = new Random().Next(10, 100);
+MaxNam(SecondNum);
+Console.WriteLine($"в числе {SecondNum} наибольшая цифра {MaxNam(SecondNum)}");
---
+++ 2_lesson/1-4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+
+
+void OneOfNum(int num)
+{       
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");        
+    }    
+}
+int a = new Random().Next(0,300);
+int b = new Random().Next(300,1000);
+Console.WriteLine($"Даны числа {a} и {b}. Кратны ли они одновременно 7 и 23?");
+OneOfNum(a);
+OneOfNum(b);
