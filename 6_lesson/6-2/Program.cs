12fc77742fb6afa70025296f633b5f26121dadb2
Ekaterina <sevic_bag@mail.ru>
2022-07-12 22:12:44 +0300

    6-2

---
+++ 6_lesson/6-2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 2 = 10
+// 3 = 11
+// 45 = 101101
+
+void Binary(int number)
+{
+    string Dec = "";
+    while (number > 0)
+    {
+        Dec = number % 2 + Dec;
+        number /= 2;
+    }
+    Console.Write(Dec);
+}
+
+Binary(45);
