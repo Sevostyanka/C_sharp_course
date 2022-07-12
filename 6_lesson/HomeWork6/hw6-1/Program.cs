880f709eb334c8d0de91ddc6062d8bb8e083c897
Ekaterina <sevic_bag@mail.ru>
2022-07-12 23:29:48 +0300

    hw6-1

---
+++ 6_lesson/HomeWork6/hw6-1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    Console.Write($"В массиве: ");
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+void PositiveAmount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]>0) count++;
+    }
+    Console.WriteLine($"Количество чисел больше 0: {count}");
+}
+
+int a = int.Parse(Console.ReadLine());
+int[] mass = MassNums(a);
+Print(mass);
+PositiveAmount(mass);
