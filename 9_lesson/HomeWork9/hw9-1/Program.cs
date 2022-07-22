7f6362292c11b27a723bb3e83af21a7a3d7016be
Ekaterina <sevic_bag@mail.ru>
2022-07-22 21:39:47 +0300

    9_lesson

---
+++ 9_lesson/HomeWork9/hw9-1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.WriteLine("Задайте меньшее значениe:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте большее значение:");
+int n = int.Parse(Console.ReadLine());
+NatNumbers(m,n);
+
+void NatNumbers (int m, int n)
+{
+    if (n < m) return;
+    NatNumbers(m,n-1);
+    Console.Write($"{n}, ");
+}
---
+++ 9_lesson/HomeWork9/hw9-2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Задайте меньшее значениe:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте большее значение:");
+int n = int.Parse(Console.ReadLine());
+
+int Sum(int m, int n)
+{
+    if( n <= m ) return m;
+    return Sum(m, n-1)+ n;
+}
+
+Console.WriteLine($"Ответ: {Sum(m,n)}");
